cbef96ec50411e9faee4cb0363fe8bc5b51da246
Anirudha-D <84765522+Anirudha-D@users.noreply.github.com>
2021-06-25 09:56:49 +0530

    Update Program-7.c

--- Program-7.c
+++ Program-7.c
@@ -3,17 +3,16 @@
 
 //INSERT THE MISSING CODE 
 #include<stdio.h>
- int main()
+int main()
 {
  int a[10],i,n;
  printf("Enter size of the array and the array elements");
  scanf("%d",&n);
-  
+ 
  for(i=0;i<n;i++)
  scanf("%d",&a[i]);
  printf("Array elements in reverse order : ");
  for(i=n-1;i>=0;i--) 
- printf("%d",a[i]);
+ printf("%d ",a[i]);
  return(0);
 }
-
